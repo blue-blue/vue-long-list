a95c878b949de74a45584a430f45f049a08f2e4e
楚呈祥 <chu_ccx@126.com>
2021-05-28 16:13:51 +0800

    最大容积

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ body {
   width: 100%;
 }
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="news-wrapper">
     <!-- <item v-for="item in list" :newsItem="item" :key="item.id" /> -->
-    <router-link to="/">
-        <!-- 显示一条新闻 -->
-        <div class="one-new" v-for="item in list" :key="item.id">
+    <div class="scroll-container" ref="scrollContainer" @scroll.passive="handleScroll">
+      <!-- 根据待显示新闻列表数据，显示新闻列表 -->
+      <div v-for="item in showDataList" :key="item.id">
+        <router-link class="one-new" to="/">
           <!-- 新闻左侧标题、评论、来源部分 -->
           <div class="new-left">
             <h3>{{ item.title }}</h3>
@@ -20,8 +21,9 @@
           <div class="new-right">
             <img :src="item.image" alt="PIC" />
           </div>
-        </div>
-      </router-link>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,20 +38,51 @@ export default {
   },
   data () {
     return {
-      list: []
+      // 全部数据
+      allDataList: [],
+      // 单条数据高度
+      itemHeight: 100,
+      // 列表最大容积
+      containSize: 0,
+      // 容器第一元素的索引
+      startIndex: 0
+    }
+  },
+  async created () {
+    this.getList(10)
+  },
+  computed: {
+    // 容器最后一个元素的索引
+    endIndex () {
+      let endIndex = this.startIndex + this.containSize
+      if (!this.allDataList[endIndex]) {
+        endIndex = this.allDataList.length - 1
+      }
+      return endIndex
+    },
+    // 待显示的数据
+    showDataList () {
+      return this.allDataList.slice(this.startIndex, this.endIndex)
     }
   },
   mounted () {
-    this.getList(10)
+    console.log(this.$refs.scrollContainer.offsetHeight)
+    this.containSize = ~~(this.$refs.scrollContainer.offsetHeight / this.itemHeight) + 2
+    console.log(this.containSize)
   },
   methods: {
+    // 获取列表数据
     getList (num) {
       const url = `/newslist?pageNo=${num}`
       axios.get(url).then((res) => {
         if (res.status === 200) {
-          this.list = res.data.data.result
+          this.allDataList = res.data.data.result
         }
       })
+    },
+    // 定义滚动行为事件方法
+    handleScroll () {
+      console.log(this.$refs.scrollContainer.scrollTop)
     }
   }
 }
@@ -57,12 +90,30 @@ export default {
 <style lang="scss" scoped>
 .news-wrapper {
   width: 100%;
-  max-width: 800px;
+  // max-width: 800px;
   height: 100%;
   a {
-    display: block;
-    width: 100%;
+    // display: block;
+    // width: 100%;
     text-decoration: none;
+  }
+  .scroll-container {
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+    // .warpper {
+    //   height: 100%;
+    // }
+    .loading {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: center;
+      align-items: center;
+      height: 60px;
+      color: #2d8cf0;
+    }
   }
   .one-new {
     display: flex;
